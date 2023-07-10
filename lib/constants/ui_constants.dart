4f17eb0e2c67453f1384e01e965f621c213f391f
stenin <steninkgeorge007@gmail.com>
2023-07-10 22:07:01 +0530

    login_view

---
+++ lib/constants/ui_constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tweet/theme/palette.dart';
+
+import 'constants.dart';
+
+class UIConstants {
+  static AppBar appBar() {
+    return AppBar(
+      title: SvgPicture.asset(
+        AssetsConstants.twitterLogo,
+        color: Pallete.blueColor,
+      ),
+    );
+  }
+}
--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tweet/constants/ui_constants.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,6 +11,8 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: UIConstants.appBar(),
+    );
   }
 }
