8bf9c2b8b274f633729e1050d15f8821d380945f
stenin <steninkgeorge007@gmail.com>
2023-07-10 21:34:49 +0530

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '64ac1c859b4b75360aea';
+  static const String projectId = '64ac19a20b4999dc324d';
+  static const String endPoint = 'http://localhost:80/v1';
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tweet/features/auth/view/login_view.dart';
+import 'package:tweet/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo', theme: AppTheme.theme, home: LoginView());
+  }
+}
