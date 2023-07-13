5c9b979ad8cb6e532354c5f32e083d47c0603697
stenin <steninkgeorge007@gmail.com>
2023-07-13 23:57:24 +0530

    signup api ready

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,11 +1,38 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart' as models;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:tweet/core/core.dart';
 
-//want to signup , use model.user
-//want to signin , use account.service
+//want to signup , use service account
+//want to signin , use model account
 //to acess user data, use model.user
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthAPI(account: account);
+});
 
 abstract class IAuthAPI {
   FutureEither<models.User> signUp(
-      {required String email, required String passowrd});
+      {required String email, required String password});
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+  @override
+  FutureEither<models.User> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+          userId: ID.unique(), email: email, password: password);
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? "some unexpected error occured", stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
 }
---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthController extends StateNotifier {
+  AuthController() : super(false);
+
+  //isLoading
+}
