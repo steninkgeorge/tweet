35475574a0b541d8cec8aed2a80ab64fb0e95bcd
stenin <steninkgeorge007@gmail.com>
2023-07-14 00:16:13 +0530

    signup view controller

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tweet/apis/auth_api.dart';
+import 'package:tweet/core/core.dart';
 
-class AuthController extends StateNotifier {
-  AuthController() : super(false);
+final authControllerProvider =
+    StateNotifierProvider<AuthController, bool>((ref) {
+  return AuthController(authAPI: ref.watch(authAPIProvider));
+});
 
-  //isLoading
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authAPI;
+  AuthController({required AuthAPI authAPI})
+      : _authAPI = authAPI,
+        super(false);
+
+  //state=isLoading
+
+  void signUp(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    state = true;
+    final res = await _authAPI.signUp(email: email, password: password);
+
+    res.fold((l) => showSnackBar(context, l.message), (r) => null);
+  }
 }
