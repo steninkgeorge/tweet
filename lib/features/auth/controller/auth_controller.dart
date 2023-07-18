8678f0d631b308215fe5b48ad2bbb747ff04631f
stenin <steninkgeorge007@gmail.com>
2023-07-18 20:47:52 +0530

    home screen setup

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tweet/apis/auth_api.dart';
 import 'package:tweet/core/core.dart';
+import 'package:tweet/features/auth/view/login_view.dart';
+import 'package:tweet/features/home/view/home_view.dart';
 
 final authControllerProvider =
     StateNotifierProvider<AuthController, bool>((ref) {
@@ -23,7 +25,12 @@ class AuthController extends StateNotifier<bool> {
     state = true;
     final res = await _authAPI.signUp(email: email, password: password);
     state = false;
-    res.fold((l) => showSnackBar(context, l.message), (r) => print(r.email));
+    res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) => {
+              showSnackBar(context, "Account created , Please login"),
+              Navigator.push(context, LoginView.route())
+            });
   }
 
   void login(
@@ -33,6 +40,7 @@ class AuthController extends StateNotifier<bool> {
     state = true;
     final res = await _authAPI.login(email: email, password: password);
     state = false;
-    res.fold((l) => showSnackBar(context, l.message), (r) => print(r.userId));
+    res.fold((l) => showSnackBar(context, l.message),
+        (r) => Navigator.push(context, HomeView.route()));
   }
 }
